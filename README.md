c03c2f084120b445ce20b12fc791cbbfe8887e8c
jasusetala <jasu@Jasus-MacBook-Pro.local>
2016-11-12 13:51:54 +0200

    Update Nov 12 2016

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # saaHaku
 
 Yksinkertainen JavaScript-plugin, jolla saadaan haettua paikallissää Ilmatieteen laitoksen tai Forecan internetsivuilta.
+
+## How to use
+
+```HTML:
+<link rel="stylesheet" href="saaHaku.css">
+...
+<script src="saaHaku.js"></script>
+...
+<form class="saahaku" name="saahaku">
+    <select name="source">
+        <option value="ilmatieteenlaitos">Ilmatieteen laitos</option>
+        <option value="foreca">Foreca</option>
+    </select>
+    <input type="text" placeholder="Paikkakunta" name="location"/>
+    <input type="submit" value="Hae" onclick="saaHaku()"/>       
+</form>```
--- saaHaku.js
+++ saaHaku.js
@@ -2,9 +2,13 @@
 function saaHaku() {
     var source = document.saahaku.source.value;
     var location = document.saahaku.location.value;
-    if (source === 'ilmatieteenlaitos') {
-        window.open("http://ilmatieteenlaitos.fi/saa/" + location, "_blank");
+    if (location === '') {
+        alert("Syötä paikkakunta.");
     } else {
-        window.open("http://www.foreca.fi/Finland/" + location, "_blank");
+        if (source === 'ilmatieteenlaitos') {
+            window.open("http://ilmatieteenlaitos.fi/saa/" + location, "_blank");
+        } else {
+            window.open("http://www.foreca.fi/Finland/" + location, "_blank");
+        }
     }
 }
